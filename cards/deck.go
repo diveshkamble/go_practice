471a212e5e9b1adfa9bd33722576a25df5b0e058
Divesh Kamble <diveshkamble@gmail.com>
2023-05-27 14:29:28 +0530

    Implemented newDeck() function

---
+++ cards/deck.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// Create a new type of deck
+// which is a slice of strings
+
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamond", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, values := range cardValues {
+			deckCard := values + " of " + suit
+			cards = append(cards, deckCard)
+		}
+	}
+	return cards
+}
---
+++ helloworld/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// main packages are executable
+// 'main' package must always contain func main
+func main() {
+	fmt.Println("Hello World!")
+}
